57742fe6f4974999e97262d5c4620be61ec86781
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-10-17 19:23:42 -0300

    add event delegation for updating display

--- main.js
+++ main.js
@@ -1,13 +1,44 @@
+// operations
 const addButton = document.querySelector(".add");
 const subtractButton = document.querySelector(".subtract");
+const multiplyButton = document.querySelector(".multiply");
+const divideButton = document.querySelector(".divide");
+// display
+const display = document.querySelector(".display");
+// numbers
 
-// Find method to avoid repeating this code for each button
+const allButtons = document.querySelector(".numButtonsContainer");
+
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+
+// event delegation to avoid repeating event listeners. Use for operations?
+allButtons.addEventListener("click", (event) => {
+  let displayValue = event.target.value;
+
+  console.log(displayValue);
+});
+
 addButton.addEventListener("click", () => {
   console.log(operate(addition, 8, 2));
 });
 
 subtractButton.addEventListener("click", () => {
   console.log(operate(subtract, 8, 2));
+});
+
+multiplyButton.addEventListener("click", () => {
+  console.log(operate(multiply, 8, 2));
+});
+divideButton.addEventListener("click", () => {
+  console.log(operate(divide, 8, 2));
 });
 
 function addition(a, b) {
@@ -26,6 +57,8 @@ function divide(a, b) {
   return a / b;
 }
 
+function displayWindow() {}
+
 function operate(operator, a, b) {
   switch (operator) {
     case addition:
@@ -38,5 +71,3 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
-
-// console.log(operate(divide, 20, 4));
